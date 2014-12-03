735a8ecaa5ad4c86befe762942a1a103921352b3
Alexander Proschenko <alexpp@inbox.ru>
2014-12-03 18:55:11 +0300

    new sketches

--- sketches/bt_4led/bt_4led.ino
+++ sketches/bt_4led/bt_4led.ino
@@ -3,13 +3,20 @@
 enum LedState { LED_ON, LED_OFF };
 boolean isFadeMode = false;
 
-#define LED_CNT 6
-int ledPins[LED_CNT] = {11, 10, 9, 6, 5, 3}; // pwm pins
+#define LED_CNT 4
+int ledPins[LED_CNT] = {9, 6, 5, 3}; // pwm pins
 
 LedState led_states[LED_CNT];
+int led_level[LED_CNT];
 
 #define rxPin 2
 #define txPin 4
+
+#define CMD_STEPS 6
+char forwardPrefix = 'A';
+char backwardPrefix = (char)((int)forwardPrefix + CMD_STEPS);
+char leftPrefix = (char)((int)backwardPrefix + CMD_STEPS);
+char rightPrefix = (char)((int)leftPrefix + CMD_STEPS);
 
 // set up a new serial port
 SoftwareSerial mySerial =  SoftwareSerial(rxPin, txPin);
@@ -20,6 +27,7 @@ void setup()
   {
     pinMode(ledPins[i], OUTPUT); //set pwm pins to output
     led_states[i] = LED_OFF;
+    led_level[i] = 0;
   }
   
   Serial.begin(38400);
@@ -40,7 +48,7 @@ void updateState()
     }
     else
     {
-      analogWrite(ledPins[i], 255);
+      analogWrite(ledPins[i], led_level[i]);
     }
   }
 }
@@ -52,7 +60,6 @@ void loop()
   {
     char c = mySerial.read();
     Serial.println(c);
-    delay(10);
     command.concat(c);
   }
   
@@ -61,7 +68,6 @@ void loop()
     return;
 
   Serial.println("command is -> " + command);
-  Serial.println(command.length());
 
   if (command == "ab")
   {
@@ -90,9 +96,46 @@ void loop()
     led_states[3] = led_states[3] == LED_OFF ? LED_ON : LED_OFF;
     Serial.println("led 3 change");
   }
+  
+  char moveCommand = command[0];
+  if (moveCommand >= forwardPrefix && moveCommand < backwardPrefix)
+  {
+    led_level[0] = (moveCommand - forwardPrefix) * (255 / (CMD_STEPS - 1));
+  }
+  if (moveCommand >= backwardPrefix && moveCommand < leftPrefix)
+  {
+    led_level[1] = (moveCommand - backwardPrefix) * (255 / (CMD_STEPS - 1));
+  }
+  if (moveCommand >= leftPrefix && moveCommand < rightPrefix)
+  {
+    led_level[2] = (moveCommand - leftPrefix) * (255 / (CMD_STEPS - 1));
+  }
+  if (moveCommand >= rightPrefix && moveCommand < (rightPrefix + CMD_STEPS + 1))
+  {
+    led_level[3] = (moveCommand - rightPrefix) * (255 / (CMD_STEPS - 1));
+  }
 
-  char charBuf[1000];
-  command.toCharArray(charBuf, 1000);
+  if (moveCommand == 'G' || moveCommand == 'A')
+  {
+    led_level[0] = led_level[1] = 0;
+  }
+  if (moveCommand == 'S' || moveCommand == 'M')
+  {
+    led_level[2] = led_level[3] = 0;
+  }
+
+  Serial.print("forward ");
+  Serial.println(led_level[0]);
+  Serial.print("back ");
+  Serial.println(led_level[1]);
+  Serial.print("left ");
+  Serial.println(led_level[2]);
+  Serial.print("right ");
+  Serial.println(led_level[3]);
+
+  command = "#" + command + "*";
+  char charBuf[5];
+  command.toCharArray(charBuf, 5);
   mySerial.write(charBuf);
   mySerial.flush();
  
